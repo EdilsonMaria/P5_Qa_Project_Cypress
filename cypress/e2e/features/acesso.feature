2b66dbafc10ce182a149a9d68e2773e5b7ec6377
EdilsonMaria <edilson.eng1@gmail.com>
2025-06-04 23:51:01 -0300

    Criação do cenario acesso

---
+++ cypress/e2e/features/acesso.feature
@@ -0,0 +1,8 @@
+#language: pt
+
+Funcionalidade: Acessar informações
+
+Cenário: ver as informações além da página incial
+    Dado acesso ao site Meu imposto de Renda
+    Quando clico no "Mais serviços..."
+    Então deve aparecer a tela que contém Mais serviços
---
+++ cypress/e2e/steps/acessoSteps.js
@@ -0,0 +1,29 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Given("acesso ao site Meu imposto de Renda", () => {
+  cy.visit("https://www.gov.br/receitafederal/pt-br/assuntos/meu-imposto-de-renda");
+
+  cy.contains("button", "Aceitar todos", { timeout: 10000 })
+    .should("be.visible")
+    .click();
+
+  cy.get(
+    "#modal-tutorial > .vertical-alignment-helper > .modal-dialog > .modal-content > :nth-child(1) > :nth-child(2) > :nth-child(2) > .col-lg-12 > .br-button",
+    { timeout: 10000 }
+  )
+    .should("be.visible")
+    .click();
+});
+
+When('clico no "Mais serviços..."', () => {
+  cy.get(':nth-child(2) > :nth-child(1) > .flipcard > .flipcard-wrap > .card-front')
+    .should("be.visible")
+    .click();
+});
+
+Then(
+  "deve aparecer a tela que contém Mais serviços",
+  () => {
+    cy.contains('Despesas Públicas');
+  }
+);
