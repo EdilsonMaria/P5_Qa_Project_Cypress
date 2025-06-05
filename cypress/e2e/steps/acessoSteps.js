2225a562958948ebde5ffcd97147940c2d761cd6
EdilsonMaria <edilson.eng1@gmail.com>
2025-06-04 23:58:22 -0300

    Refatoração do cenario acesso

--- cypress/e2e/steps/acessoSteps.js
+++ cypress/e2e/steps/acessoSteps.js
@@ -1,4 +1,4 @@
-import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
 
 Given("acesso ao site Meu imposto de Renda", () => {
   cy.visit("https://www.gov.br/receitafederal/pt-br/assuntos/meu-imposto-de-renda");
